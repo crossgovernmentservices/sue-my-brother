2fcb83baac00d0bd06a306f4fb0924a024cfe769
Andy Driver <andy@pagezero.net>
2016-09-07 17:08:45 +0100

    Tests for user flow through app

---
+++ tests/conftest.py
@@ -0,0 +1,153 @@
+import os
+import subprocess
+
+from flask_migrate import upgrade
+import pytest
+import sqlalchemy
+
+from app.blueprints.base.models import User
+from app.config import SQLALCHEMY_DATABASE_URI
+from app.extensions import db as _db
+from app.factory import create_app
+
+
+TEST_DATABASE_URI = SQLALCHEMY_DATABASE_URI + '_test'
+
+
+@pytest.yield_fixture(scope='session')
+def app(request):
+    app = create_app(TESTING=True, SQLALCHEMY_DATABASE_URI=TEST_DATABASE_URI)
+
+    ctx = app.app_context()
+    ctx.push()
+
+    yield app
+
+    ctx.pop()
+
+
+def reset_migrations(db):
+
+    # reset migrations, otherwise they will not be reapplied
+    conn = db.engine.connect()
+
+    try:
+        conn.execute('DELETE FROM alembic_version')
+
+    except sqlalchemy.exc.ProgrammingError:
+        pass
+
+    finally:
+        conn.close()
+
+
+def teardown_db(db_url, db):
+
+    if db_url.drivername == 'postgresql':
+        db.drop_all()
+        reset_migrations(db)
+
+    if db_url.drivername == 'sqlite':
+        if os.path.exists(db_url.database):
+            os.unlink(db_url.database)
+
+
+def init_db(db_url, db):
+    sqlalchemy.orm.configure_mappers()
+
+    try:
+        teardown_db(db_url, db)
+
+    except sqlalchemy.exc.OperationalError as e:
+        if 'does not exist' in str(e):
+            create_db(db_url)
+            init_db(db_url, db)
+
+    upgrade()
+
+
+def create_db(db_url):
+    dbname = db_url.database
+
+    if db_url.drivername == 'postgresql':
+        subprocess.call(['/usr/bin/env', 'createdb', dbname], timeout=1)
+
+    if db_url.drivername == 'sqlite':
+        # created automatically by migrations
+        pass
+
+
+@pytest.yield_fixture(scope='session')
+def db(request, app):
+    _db.app = app
+    db_url = sqlalchemy.engine.url.make_url(TEST_DATABASE_URI)
+
+    init_db(db_url, _db)
+
+    yield _db
+
+    teardown_db(db_url, _db)
+
+
+@pytest.yield_fixture(scope='function')
+def db_session(db, request):
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    session_factory = sqlalchemy.orm.sessionmaker(bind=connection)
+    db.session = session = sqlalchemy.orm.scoped_session(session_factory)
+
+    yield session
+
+    transaction.rollback()
+    connection.close()
+    session.remove()
+
+
+@pytest.fixture
+def selenium(db, live_server, selenium):
+    """Override selenium fixture to always use flask live server"""
+    return selenium
+
+
+@pytest.fixture
+def test_user(db_session):
+    user = User(email='test@example.com', name='Test Test', active=True)
+    db_session.add(user)
+    db_session.commit()
+    return user
+
+
+@pytest.fixture
+def unnamed_user(db_session):
+    user = User(email='test@example.com', active=True)
+    db_session.add(user)
+    db_session.commit()
+    return user
+
+
+@pytest.yield_fixture
+def login(client):
+
+    def do_login(user):
+        with client.session_transaction() as session:
+            session['user_id'] = user.id
+            session['_fresh'] = True
+
+    yield do_login
+
+    with client.session_transaction() as session:
+        del session['user_id']
+        del session['_fresh']
+
+
+@pytest.yield_fixture
+def unnamed_user_logged_in(unnamed_user, login):
+    login(unnamed_user)
+    yield unnamed_user
+
+
+@pytest.yield_fixture
+def logged_in(test_user, login):
+    login(test_user)
+    yield test_user
---
+++ tests/test_user_flow.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Test user flow
+"""
+
+from bs4 import BeautifulSoup
+from flask import url_for
+import pytest
+
+
+def request(url, method, data=None):
+    r = method(url, data=data)
+    r.soup = BeautifulSoup(r.get_data(as_text=True), 'html.parser')
+    return r
+
+
+@pytest.fixture
+def index(client):
+    return request(url_for('base.index'), client.get)
+
+
+@pytest.fixture
+def details_form(client):
+    return request(url_for('base.details'), client.get)
+
+
+@pytest.fixture
+def post(client):
+
+    def submit(url):
+
+        def response(data):
+            return request(url, client.post, data=data)
+
+        return response
+
+    return submit
+
+
+@pytest.fixture
+def post_details(post):
+    return post(url_for('base.details'))
+
+
+@pytest.fixture
+def post_suit(post):
+    return post(url_for('base.start_suit'))
+
+
+@pytest.mark.usefixtures('db_session')
+class WhenGettingStarted(object):
+
+    def it_shows_a_call_to_action(self, index):
+        links = index.soup.find_all('a', href=url_for('base.details'))
+        assert 'Get started' in links[0].text
+
+    def it_requests_user_details_if_unrecognized(self, details_form):
+        heading = details_form.soup.find('h1')
+        assert heading.text == 'Enter your details'
+
+
+class WhenEnteringPlaintiffDetails(object):
+
+    def it_redirects_if_authn_user_has_name(self, logged_in, details_form):
+        assert details_form.status_code == 302
+
+    def it_prepopulates_authn_users_email(
+            self, unnamed_user_logged_in, details_form):
+        print(details_form.soup)
+        email = details_form.soup.find('input', id='email')
+        assert email['value'] == unnamed_user_logged_in.email
+
+    def it_requires_a_full_name(self, post_details):
+        errors = post_details({}).soup.find_all(class_='error')
+        assert errors[0].parent.parent.find('label').text == 'Full Name'
+
+    def it_requires_an_email_address(self, post_details):
+        data = {'name': 'Test User'}
+        errors = post_details(data).soup.find_all(class_='error')
+        assert errors[0].parent.parent.find('label').text == 'Email Address'
+
+
+class WhenEnteringSuitDetails(object):
+
+    def it_requires_defendant_name(self, post_suit):
+        errors = post_suit({}).soup.find_all(class_='error')
+        assert errors[0].parent.parent.find('label').text == \
+            'Your Brother\'s Full Name'
